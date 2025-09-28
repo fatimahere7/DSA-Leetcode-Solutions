7ccd41f5dc704c043b5637186e29248546a58951
Fatima Mansoor <fatima.mansoor@nexquery.com>
2025-09-29 02:08:13 +0500

    longest substring LC3

--- LC3_longest_substring.py
+++ LC3_longest_substring.py
@@ -1,27 +1,31 @@
-# map = {}
-# for i in range(len(arr)):
-#     if arr[i] in map:
-#         print(arr[i])
-#     map[arr[i]]= True
-def longest_substring(arr):
+# Given a string s, find the length of the longest substring without duplicate characters.
+
+
+# Example 1:  Input: s = "abcabcbb"  Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+# Example 2: Input: s = "bbbbb"  Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3: Input: s = "pwwkew"
+# Output: 3: Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+
+#sliding window
+def longestSubstring(s:str)->str:
+    l = 0
+    set_map = set()
+    max_len = 0
+    for r in range(len(s)):
+        while s[r] in set_map:
+            set_map.remove(s[l])
+            l += 1
+        set_map.add(s[r])    
+        max_len = max(max_len, r-l+1)
     
-    for i in range(len(arr)):
-        start = 0
-        end = 0
-        max_len = 0
-        map = {}
-        while end < len(arr):
-            if arr[end] not in map:
-                map[arr[end]]= end
-                end += 1
-                max_len = max(max_len, len(map))
-            else:
-                map.pop(start)     
-                start += 1
-        print("Max lenghth is :"+ max_len)        
+    return max_len    
         
-
-
-
-string = "abcdabda"
-longest_substring(string)        
+    
+print(longestSubstring("pwwkew"))
+print(longestSubstring("bbbbbb"))
+print(longestSubstring("abcadfsa"))
