45be5667ce735ee7ae867cfd59675ecb1f808332
Fatima Mansoor <fatima.mansoor@nexquery.com>
2025-09-30 04:13:21 +0500

    Fix palindrome logic

--- LC5_plandrome.py
+++ LC5_plandrome.py
@@ -4,7 +4,7 @@ def plandromeStr(s:str) ->str:
     res = ""
     maxLen = 0
     for i in range(len(s)):
-        
+        # odd
         left , right = i , i
         while left >= 0 and right < len(s) and s[left] == s[right]:
             if (right - left + 1) > maxLen:
@@ -12,7 +12,7 @@ def plandromeStr(s:str) ->str:
                 maxLen = right - left + 1
             left-=1
             right+=1
-            
+        #even 
         left, right = i, i+1
         while left >= 0 and right < len(s) and s[left] == s[right]:
             if (right - left + 1) > maxLen:
