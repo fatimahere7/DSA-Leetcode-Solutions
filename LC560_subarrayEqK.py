692f22d3fc5b62d7101400f8c0e1b5015768b94a
Fatima Mansoor <fatima.mansoor@nexquery.com>
2025-10-02 22:33:05 +0500

    SubArray sum equals k with bruteforce and prefixsum &hashmap

--- LC560_subarrayEqK.py
+++ LC560_subarrayEqK.py
@@ -13,4 +13,24 @@ def subArrayEqKBF(nums , tar):
             
     return count
 
+#prefix sum
+def subArrayEqkPS(nums, k):
+    count = 0
+    prefixSum = 0
+    hashmap = {0:1}  #{prefixSum:frequency}
+    for i in range(len(nums)):
+        prefixSum += nums[i]
+        if (prefixSum - k) in hashmap:
+            count += hashmap[prefixSum-k]
+        
+        if prefixSum in hashmap:
+                hashmap[prefixSum] += 1
+        else:
+            hashmap[prefixSum] = 1  
+    return count                  
+            
+
+
 print(subArrayEqKBF([1,9,4,20,3,10,5] , 33)) 
+print(subArrayEqkPS([1, 9,4,20,3,10,5] , 33)) 
+print(subArrayEqkPS([1, -1, 1, -1, 1], 0))
