a85fa1fa0b52ac10f855e8580479c6573bbc7608
Fatima Mansoor <fatima.mansoor@nexquery.com>
2025-10-14 05:32:45 +0500

    Resursion practice

---
+++ recursion.py
@@ -0,0 +1,32 @@
+
+# factorial
+def fact(n: int) -> int:
+    if n == 1:
+        return 1
+    return n * fact(n-1)
+
+
+def sum(n: int) -> int:
+    if n == 0:
+        return 0
+    return n + sum(n-1)
+
+
+def print_increasing(n):
+    if n == 0:
+        return
+    print(n)
+    print_increasing(n-1)
+    print(n)
+
+
+def reversestring(s: str) -> None:
+    if len(s) <= 1:
+        return s
+    return reversestring(s[1:]) + s[0]
+
+
+print(fact(5))
+print(sum(5))
+print_increasing(5)
+print(reversestring("hello"))
